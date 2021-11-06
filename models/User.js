b0d45d1170bf44441bf11323c4ac4359ef8f17b5
DongHyun <bomul0524@gmail.com>
2021-11-06 05:59:13 +0000

    add description about mongodb

--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken")//토큰 발행 
 //스키마 생성
 const userSchema = mongoose.Schema({
+  /*데이터를 mongodb에 저장하려면 먼저 구조(스키마)가 있어야 한다.
+  스키마는 해당 컬렉션의 문서에 어떤 종류의 값이 들어가는지를 정의한다.
+  mongoose 모듈을 불러와 mongoose.Schema 를 통해 스키마 객체를 생성한다.*/
+  
   name: {
     type: String,
     maxlength: 50
@@ -34,68 +38,71 @@ const userSchema = mongoose.Schema({
   }
 })
 //pre는 mongoose 에서 가져온 메서드, save는 저장하기 전에 function을 실행
-userSchema.pre('save', function(next){//next는 바로 이 과정을 pass 함
+userSchema.pre('save', function(next) {//next는 바로 이 과정을 pass 함
   //현재 스키마에 들어있는 post된 password를 가져온다
   var user = this;
   //field에서 password가 변환될때만 password를 암호화 해준다.
-  if(user.isModified('password')){
+  if (user.isModified('password')) {
     //bcrypt 패키지의 salt를 이용해서 비밀번호를 암호화 시킨다.
     //genSalt는 salt를 생성한다
-    bcrypt.genSalt(saltRounds, function(err, salt){
-    if(err) return next(err)
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err)
 
-    bcrypt.hash(user.password, salt, function(err, hash){
-      if(err) return next(err)
-      user.password = hash//password를 암호화된 hash 로 바꿔준다
-      next()//완료 후 돌아감
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err)
+        user.password = hash//password를 암호화된 hash 로 바꿔준다
+        next()//완료 후 돌아감
+      })
     })
-  })
   } else {//그냥 나갈 곳을 만들어준다.
     next()
   }
 });
 
-userSchema.methods.comparePassword = function(plainPassword, callback){
+userSchema.methods.comparePassword = function(plainPassword, callback) {
   //plainPassword vs hashed password (using bcrypt method)
-  bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-    if(err) return callback(err)//different
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if (err) return callback(err)//different
     callback(null, isMatch)//same
   })
 }
 
-userSchema.methods.generateToken = function(callback){
+userSchema.methods.generateToken = function(callback) {
   var user = this;
   //jsonwebtoken을 이용해서 token 생성
-  var token = jwt.sign(user._id.toHexString(), 'secretToken');//임의로 두번째 지정
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');//임의로 두번째(secret key) 지정
   //user._id + 'secretToken' = token (incode)
   //->
   //'secretToken' -> user._id (decode)
-  
+
   user.token = token;//user 객체의 token 에 토큰을 넣어준다.
-  user.save(function(err, user){
-    if(err) return callback(err)
+  user.save(function(err, user) {
+    if (err) return callback(err)
     callback(null, user)
   })
 }
 
-userSchema.statics.findByToken = function(token, callback){
+userSchema.statics.findByToken = function(token, callback) {
   var user = this;
 
   //토큰을 복호화(decode) 한다
-  jwt.verify(token, 'secretToken', function(err, decoded){
+  jwt.verify(token, 'secretToken', function(err, decoded) {
     //유저 아이디(decoded)를 이용해서 유저를 찾은 다음에
     //클라이언트에서 가져온 token 과 DB에 보관된 토큰이 일치하는지 확인
 
-    user.findOne({"_id": decoded, "token": token }, function(err, user){
+    user.findOne({ "_id": decoded, "token": token }, function(err, user) {
 
-      if(err) return callback(err);
+      if (err) return callback(err);
       callback(null, user);
     })
   })
 }
 
-//스키마를 모델로 감싸준다.
-//모델은 스키마를 감싸주는 역할
-const User = mongoose.model('User', userSchema)
+//스키마를 모델로 감싸준다. 모델은 스키마를 감싸주는 역할
+/*module.exports 를 통해 mongoose의 모델로 User를 사용할 수 있도록 한다. 아래 과정을 통해 User모델은 userSchema 에 접근할 수 있게 된다.
 
-module.exports = { User }//모델 다른 곳에서도 쓸 수 있게 export 해준다.
+model 함수에선 기본적으론 두개의 파라미터를 필요로한다. 첫번째는 파라미터는 해당 스키마의 이름이고, 두번째는 스키마 객체이다. 스키마의 이름을 정해주면, 이의 복수형태로 컬렉션이름을 만들어준다.(현 users)
+
+예를들어, Book 으로 설정한다면, 실제 데이터베이스에서 생성되는 컬렉션 이름은 books 이다. 그리고, BookInfo 로 설정한다면 bookinfos 로 만들어진다.*/
+module.exports = mongoose.model('User', userSchema)
+//모델 다른 곳에서도 쓸 수 있게 export 해준다.
