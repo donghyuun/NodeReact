3f35281699b2cab426fb07b86a6837240c56c6ac
DongHyun <bomul0524@gmail.com>
2021-11-13 13:55:58 +0000

    add redux&middleware

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import user from "./user_reducer";
+
+//rootReducer에서 combineReducers를 통해 reducer들을 하나로 합쳐준다.
+const rootReducer = combineReducers({
+	//
+})
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "antd/dist/antd.css";
+import {Provider} from "react-redux";
+import {applyMiddleware} from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
+//원래 그냥 createStore 만 적으면 되는데 추가 기능을 위해 미들웨어를 추가해준다
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/*이부분에 App대신 보여주고픈 컴포넌트를 넣으면 된다.*/ />
-  </React.StrictMode>,
+	<Provider store={createStoreWithMiddleware(Reducers,
+			//브라우저에서 DEVTOOLS 익스텐션을 사용하기 위함
+			window.__REDUX_DEVTOOLS_EXTENSTION__ &&
+		    window.__REDUX_DEVTOOLS_EXTENSTION__()
+		)}>
+		<App /*이부분에 App대신 보여주고픈 컴포넌트를 넣으면 된다.*/ />
+	</Provider>,
   document.getElementById('root')
   /*id가 root 인 태그(public/index.html에 위치) 안에 App 컴포넌트를 보여줄꺼라고 지정*/
 );
